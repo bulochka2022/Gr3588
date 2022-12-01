dde25a2b4f48b35667bd5ee2bfdb563c03a6131d
Tanya <tanya-v_92@mail.ru>
2022-12-01 20:53:07 +0300

    add solution for task 0, 1, 3, 5, 7

---
+++ Semtask1/Program.cs
@@ -0,0 +1,12 @@
+﻿String? num1line = Console.ReadLine();
+String? num2line = Console.ReadLine();
+
+if(num1line!=null&&num2line!=null)
+{
+    int num1 = int.Parse(num1line);
+    int num2 = int.Parse(num2line);
+
+    int outResult = num1*num2;
+    Console.WriteLine(outResult);
+}
+
