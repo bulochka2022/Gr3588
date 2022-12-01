f7c37a7f9a29c61bbaf4bf6c98666e4dd10098be
Tanya <tanya-v_92@mail.ru>
2022-12-01 21:00:28 +0300

    add 1,3,5,7

---
+++ Sem1Task3/Program.cs
@@ -0,0 +1,25 @@
+﻿
+string inputLine = Console.ReadLine();
+
+//Проверяем чтобы данные были не пустыми
+if (inputLine!=null)
+{
+    //Создаем массив с днями недели
+    string[] dayOfWeek = new string[7];
+    dayOfWeek[0] = "понедельник";
+    dayOfWeek[1] = "вторник";
+    dayOfWeek[2] = "среда";
+    dayOfWeek[3] = "четверг";
+    dayOfWeek[4] = "пятница";
+    dayOfWeek[5] = "суббота";
+    dayOfWeek[6] = "воскресенье";
+
+    //Парсим введенное число
+    int inputNumber = int.Parse(inputLine);
+
+    //Находим введенное число
+    string outDayOfWeek = dayOfWeek[inputNumber-1];
+
+    //Выводим данные в консоль
+    Console.WriteLine(outDayOfWeek);
+}
