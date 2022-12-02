65bcd301ae59b76ca32ea8d7e2a1864a86d3d29d
Tanya <tanya-v_92@mail.ru>
2022-12-02 12:26:51 +0300

    add solution for task 2, 4, 6, 8

---
+++ Sem1Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+//Считываем данные с консоли
+string? num1line = Console.ReadLine();
+string? num2line = Console.ReadLine();
+
+//Проверяем чтобы данные были не пустыми
+if(num1line!=null&&num2line!=null)
+{
+    //Парсим введенные числа
+    int num1 = int.Parse(num1line);
+    int num2 = int.Parse(num2line);
+
+    if(num1 > num2)
+    {
+        Console.WriteLine("max=" + num1line); //Выводим данные в консоль
+    }
+    else
+    {
+        Console.WriteLine("max=" + num2line); //Выводим данные в консоль
+    }
+}
---
+++ Sem1Task4/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+//Считываем данные с консоли
+string? num1line = Console.ReadLine();
+string? num2line = Console.ReadLine();
+string? num3line = Console.ReadLine();
+
+//Проверяем чтобы данные были не пустыми
+if(num1line!=null&&num2line!=null&&num3line!=null)
+{
+    //Парсим введенные числа
+    int num1 = int.Parse(num1line);
+    int num2 = int.Parse(num2line);
+    int num3 = int.Parse(num3line);
+
+    //Выводим данные в консоль
+    Console.WriteLine(Math.Max(num1, Math.Max(num2, num3)));
+}
