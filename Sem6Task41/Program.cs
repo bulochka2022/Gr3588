a3ba28c9224f78a23eaf0c83377f5ccc37f18c5d
Tanya <tanya-v_92@mail.ru>
2022-12-22 15:02:16 +0300

    add solution for task 39,40,41,42,43,44

---
+++ Sem6Task41/Program.cs
@@ -0,0 +1,30 @@
+﻿//  Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int number = ReadData("Сколько чисел вы хотите ввести?");
+int res = CountPosNum(number);
+PrintData(res);
+
+//Метод, считывающий данные, введенные пользователем
+ int ReadData(string msg)
+ {
+     Console.WriteLine(msg);
+     return int.Parse(Console.ReadLine() ?? "0");
+ }
+
+ //Метод, выводящий на печать данные пользователю
+ void PrintData(int res)
+ {
+     Console.WriteLine(res);
+ }
+
+//Метод для подсчета положительных чисел из чисел введенных пользователем
+ int CountPosNum(int N)
+ {
+    int res = 0;
+    while (N > 0)
+    {
+    if(ReadData("Введите число: ") > 0)res++;
+    N = N - 1;
+    }
+    return res;
+ }
---
+++ Sem6Task43/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+//y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+int b1 = ReadData("Введите точку b1:");
+int k1 = ReadData("Введите точку k1:");
+int b2 = ReadData("Введите точку b2:");
+int k2 = ReadData("Введите точку k2:");
+LineIntersection(b1, k1, b2, k2);
+
+//Метод, считывающий данные, введенные пользователем
+ int ReadData(string msg)
+ {
+     Console.WriteLine(msg);
+     return int.Parse(Console.ReadLine() ?? "0");
+ }
+
+
+//Метод, находящий пересечение двух прямых
+void LineIntersection(double b1, double k1, double b2, double k2)
+{
+double x = (b1-b2)/(k2-k1); //В первом уравнении находим x
+double y = (k2*b1-k1*b2)/(k2-k1); //Находим y, подставив найденное значение x во второе уравнение
+ 
+if (k1==k2)Console.WriteLine ("Заданные прямые не пересекаются.");
+else Console.WriteLine("Точка пересечения заданных прямых: " + (x,y));
+}
