3bfa361f99737362f2a5b290599b8f7c88b5b496
Tanya <tanya-v_92@mail.ru>
2023-01-12 11:45:59 +0300

    add solution for task 63,64,65,66,67,68,69

---
+++ Sem9Task66/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// Чтение данных из консоли
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+// Печать результата
+void PrintResult(int prefix)
+{
+    Console.WriteLine(prefix);
+}
+
+int SumMNRec(int M, int N)
+{
+    if (M >= N) return N;
+      
+    return M + SumMNRec(M + 1, N);
+}
+
+int number1 = ReadData("Введите число M: ");
+int number2 = ReadData("Введите число N: ");
+int resultLine = number1<number2? SumMNRec(number1, number2):SumMNRec(number2, number1);
+PrintResult(resultLine);
---
+++ Sem9Task68/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+// Чтение данных из консоли
+ uint ReadData(string line)
+ {
+     Console.Write(line);
+     uint number = uint.Parse(Console.ReadLine() ?? "0");
+     return number;
+ }
+
+ // Печать результата
+ void PrintResult(uint prefix)
+ {
+     Console.WriteLine(prefix);
+ }
+
+//Метод вычисления функции Аккермана с помощью рекурсии
+uint Akk(uint m, uint n) //uint - то же самое, что int, но больше нуля
+{
+    if (m == 0)
+        return n + 1;
+    else
+      if ((m != 0) && (n == 0))
+        return Akk(m - 1, 1);
+    else
+        return Akk(m - 1, Akk(m, n - 1));
+}
+ 
+ uint number1 = ReadData("Введите число M: ");
+ uint number2 = ReadData("Введите число N: ");
+ uint resultLine = Akk(number1, number2);
+ PrintResult(resultLine);
